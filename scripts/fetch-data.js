888139e5d43dc86c2d0a3979997e8a48cb0fa7d4
Gary Index <83047248+GaryIndex@users.noreply.github.com>
2025-02-08 05:13:22 +0800

    更新 fetch-data.js

--- scripts/fetch-data.js
+++ scripts/fetch-data.js
@@ -34,6 +34,8 @@ async function fetchData() {
       dates.push(currentDate.format('YYYY-MM-DD'));  // 格式化为 YYYY-MM-DD
     }
 
+    console.log(`Dates to fetch: ${dates.join(', ')}`);  // 打印所有要抓取的日期
+
     // 获取万年历数据
     const fetchCalendarData = async (date) => {
       try {
@@ -124,12 +126,13 @@ async function fetchData() {
           holidays: holidaysData,
         };
 
-        // 读取现有的 data.json 文件，如果文件为空，则初始化为空数组
+        console.log(`Daily data for ${date}:`, dailyData); // 打印当前日期的数据
+
+        // 读取现有的 data.json 文件
         let existingData = [];
         if (fs.existsSync(path)) {
           const fileData = fs.readFileSync(path, 'utf8');
           
-          // 检查文件内容是否有效
           if (fileData) {
             try {
               existingData = JSON.parse(fileData);  // 解析JSON数据
