877c8da0c3f0800f53bfce5b1332d21cf8513c8e
Gary Index <83047248+GaryIndex@users.noreply.github.com>
2025-02-11 03:47:45 +0800

    更新 generate-ics.js

--- scripts/generate-ics.js
+++ scripts/generate-ics.js
@@ -1,5 +1,5 @@
 // **处理数据**
-import { loadAllJsonData, logInfo, logError } from '/scripts/utils/utils.js'; // 确保路径正确
+import { loadAllJsonData, logInfo, logError } from './utils/utils.js'; // 确保路径正确
 
 (async () => {
   try {
