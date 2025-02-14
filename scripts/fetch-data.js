f9151829d099eed2d4b17aec87e7d4e5846fcae0
Gary Index <83047248+GaryIndex@users.noreply.github.com>
2025-02-15 05:04:56 +0800

    更新 fetch-data.js

--- scripts/fetch-data.js
+++ scripts/fetch-data.js
@@ -405,14 +405,13 @@ const fetchData = async () => {
     } catch (error) {
       console.error('数据获取或处理失败：', error.message);
     }
-
+*/
       // 打印原始数据到日志
-      await writeLog('INFO', 'calendar.json', `原始日历数据: ${JSON.stringify(calendarData, null, 2)}`);
-      await writeLog('INFO', 'astro.json', `原始星座数据: ${JSON.stringify(astroData, null, 2)}`);
-      await writeLog('INFO', 'shichen.json', `原始时辰数据: ${JSON.stringify(shichenData, null, 2)}`);
-      await writeLog('INFO', 'jieqi.json', `原始节气数据: ${JSON.stringify(jieqiData, null, 2)}`);
+      //await writeLog('INFO', 'calendar.json', `原始日历数据: ${JSON.stringify(calendarData, null, 2)}`);
+      //await writeLog('INFO', 'astro.json', `原始星座数据: ${JSON.stringify(astroData, null, 2)}`);
+      //await writeLog('INFO', 'shichen.json', `原始时辰数据: ${JSON.stringify(shichenData, null, 2)}`);
+      //await writeLog('INFO', 'jieqi.json', `原始节气数据: ${JSON.stringify(jieqiData, null, 2)}`);
       await writeLog('INFO', 'holidays.json', `原始节假日数据: ${JSON.stringify(holidaysData, null, 2)}`);
-      */
       // 使用通用的处理函数来处理原始数据（扁平化）
       const processedCalendarData = processData(calendarData, dateStr);
       const processedAstroData = processData(astroData, dateStr);
